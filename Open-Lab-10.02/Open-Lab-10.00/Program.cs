c20aa8f19812d0969f88ad19f44846b7822b196e
Lukáš Lokša <86780297+Powissen@users.noreply.github.com>
2021-11-26 10:04:47 +0100

    Function to write all data of object

---
+++ Open-Lab-10.02/Open-Lab-10.00/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Open_Lab_10._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            //LOTR.title = "Title";    
+            //Console.WriteLine(LOTR.title);
+            LOTR.Title();
+            LOTR.Pages();
+            LOTR.Cathegory();
+            LOTR.Author();
+            LOTR.ReleaseDate();
+            LOTR.WriteAllVariables();
+        }
+    }
+    public class Book
+    {
+        private string title;
+        private int pages;
+        private string cathegory;
+        private string author;
+        private string releaseDate;
+
+        public void Title() { title = "Title"; }
+        public void Pages() { pages = 1; }
+        public void Cathegory() { cathegory = "Cathegory"; }
+        public void Author() { author = "Author"; }
+        public void ReleaseDate() { releaseDate = "ReleaseDate"; }
+
+
+        public void WriteAllVariables()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Pages: " + pages);
+            Console.WriteLine("Cathegory: " + cathegory);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Release date: " + releaseDate);
+
+        }
+    }
+}
